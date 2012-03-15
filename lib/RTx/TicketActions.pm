a7acd52a3f98ee68f207d75cea3df91db3a56409
Marius Hein <marius.hein@netways.de>
2012-03-15 14:29:28 +0100

    * Created license information for all modules

--- lib/RTx/TicketActions.pm
+++ lib/RTx/TicketActions.pm
@@ -6,3 +6,27 @@ use version;
 our $VERSION="1.0.0";
 
 1;
+=pod
+
+=head1 NAME
+
+RTx::TicketActions
+
+=head1 VERSION
+
+version 1.0.0
+
+=head1 AUTHOR
+
+Marius Hein <marius.hein@netways.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by NETWAYS GmbH <info@netways.de>
+
+This is free software, licensed under:
+    GPL Version 2, June 1991
+
+=cut
+
+__END__
