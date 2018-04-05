045aaabafc72d5b609dab03a88be1a6f27910d9d
Michael Friedrich <michael.friedrich@netways.de>
2018-04-05 13:42:35 +0200

    Release version 2.0.1

--- lib/RT/Extension/TicketActions.pm
+++ lib/RT/Extension/TicketActions.pm
@@ -5,7 +5,7 @@ use strict;
 use version;
 use RT;
 
-our $VERSION='2.0.0';
+our $VERSION='2.0.1';
 
 RT->AddJavaScript('fontawesome-svg/js/fontawesome-all.min.js');
 RT->AddStyleSheets('ticketactions.css');
